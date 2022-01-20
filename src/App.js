21a06e992a13b9516c873eede3a116a6cf3ace72
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2022-01-19 19:07:09 -0500

    error handling if nothing returns from the api, or theres an error

--- src/App.js
+++ src/App.js
@@ -10,21 +10,24 @@ class App extends React.Component {
     super();
     this.state = {
       isLoggedIn: false,
-      imageFeed: []
+      imageFeed: [],
+      errorMessage: null
     }
   }
 
   componentDidMount() {
     fetch("https://api.nasa.gov/planetary/apod?api_key=iqdS8Df0jwyccz14dVTF0JIVV4vkoPWjJWdQwtek&count=10")
     .then(response => response.json())
-    .then(response => this.setState({ imageFeed: response}));
+    .then(response => this.setState({ imageFeed: response}))
+    .catch(e => this.setState({ errorMessage: e}));
 
   }
   render() {
     return (
       <div className='page'>
         <Header/>
-        <PostFeed posts = {this.state.imageFeed}/>
+        { (this.state.errorMessage || this.state.imageFeed.length === 0) ? <span>Sorry, there was an issue contecting NASA's API</span> :
+        <PostFeed posts = {this.state.imageFeed}/> }
       </div>
     )
   }
